f40630f1c6898e4363508ec0b5580aefac41ac56
mauridevcde <mauridevcde@gmail.com>
2022-11-16 16:08:42 -0300

    first commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import { AuthRoutes } from "../Auth/routes/AuthRoutes";
+import { JournalRoutes } from "../journal/routes/JournalRoutes";
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      {/* Login y Registro */}
+      <Route path="/auth/*" element={ <AuthRoutes/> }  />
+
+      {/* Journal App */}
+      <Route path="/*"  element={ <JournalRoutes/> } />
+    </Routes>
+  );
+};
